38aba3921e929d61d0c6f03e1533e78f63c29765
sameermcfc <sameer.mahmud1@hotmail.com>
2025-01-07 23:43:16 +1100

    update about me

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -17,7 +17,13 @@ export default function DocsPage() {
             width={270}
           />
         <div className="inline-block max-w-lg text-center justify-center">
-          <p>I am currently a final year student at UNSW studying Comp Sci/Commerce. I enjoy maths, science and the business world, continuously applying new knowledge into my personal and uni projects. Some of my favorite hobbies include volunteering with the local community, working on my car and watching anime.</p>
+          <p>I am a dedicated software developer with over two years of experience at Optus, where I’ve honed my technical skills in building reliable, robust, and user-friendly software solutions. Collaborating closely with my team, I thrive in creating impactful technologies that deliver real value to users.
+
+I graduated from UNSW in 2023 with a double degree in Computer Science and Commerce. My passion for maths, science, and business continues to drive me as I apply my knowledge to both professional and personal projects, with a particular interest in emerging technologies like Web3, blockchain, and artificial intelligence.
+
+Beyond my professional career, I’m deeply committed to giving back to the community. I work with various organizations, particularly those focused on youth education, sports, and community engagement.
+
+With a blend of technical expertise, business acumen, and community spirit, I strive to stay at the forefront of innovation while making a positive impact in everything I do.</p>
         </div>
       </section>
     </DefaultLayout>
